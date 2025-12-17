4a5e2db1ffe7233effe8d56be6c3a5a243bdede9
KosukeIsogai <kouiso@ritmo.co.jp>
2025-12-17 19:22:08 +0900

    refactor: 投稿のアーカイブページ作成機能を削除

--- wp-content/themes/muashi/functions.php
+++ wp-content/themes/muashi/functions.php
@@ -108,18 +108,6 @@ function all_modified_date( $post_type = "post", $format = "Y-m-d H:i:s" ){
     return $all_modified_date;
 }
 
-// 投稿のアーカイブページを作成する
-function post_has_archive($args, $post_type)
-{
-    if ('post' == $post_type) {
-        $args['rewrite'] = true; // リライトを有効にする
-        $args['has_archive'] = 'news'; // 任意のスラッグ名
-    }
-    return $args;
-}
-add_filter('register_post_type_args', 'post_has_archive', 10, 2);
-
-
 /**
  * メディア投稿タイプ
  */
