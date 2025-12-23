d7df654ed319b9885e11af8112fcadc43867c46f
KosukeIsogai <kouiso@ritmo.co.jp>
2025-12-23 19:28:15 +0900

    fix: history系ページのサイドバーをsidebar_historyに統一し、インタビューメニュー修正スクリプトを追加

--- wp-content/themes/muashi/page-history-innovation.php
+++ wp-content/themes/muashi/page-history-innovation.php
@@ -17,37 +17,7 @@ $history_image_base = get_theme_file_uri( '/assets/img/history' );
         <div class="page__bg-sub"></div>
     </div>
 
-    <div class="navigation">
-        <div class="navigation__inner">
-            <ul class="navigation__list">
-                <li class="navigation__item">
-                    <a href="<?= URL_ABOUT_US ?>" class="navigation__item-title">
-                    企業情報
-                    </a>
-                </li>
-                <li class="navigation__item">
-                    <a href="<?php echo URL_HISTORY; ?>" class="navigation__item-title">
-                    ヒストリー
-                    </a>
-                </li>
-                <li class="navigation__item">
-                    <a href="<?php echo URL_COMPANY; ?>" class="navigation__item-title">
-                    会社概要
-                    </a>
-                </li>
-                <li class="navigation__item">
-                    <a href="<?php echo URL_STORY; ?>" class="navigation__item-title">
-                    ストーリー
-                    </a>
-                </li>
-                <li class="navigation__item">
-                    <a href="<?php echo URL_VOICE; ?>" class="navigation__item-title">
-                    お客様の声
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <?php muashi_render_sidebar_navigation( 'sidebar_history' ); ?>
 
     <div class="page__wrapper">
         <div class="page__container">
