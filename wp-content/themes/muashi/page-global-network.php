7681f3981ca30e6cf18ca2c1cbe6a6f43d4f526f
KosukeIsogai <kouiso@ritmo.co.jp>
2025-12-23 14:17:35 +0900

    グローバルネットワークページにページネーションを追加

--- wp-content/themes/muashi/page-global-network.php
+++ wp-content/themes/muashi/page-global-network.php
@@ -6,10 +6,14 @@ global $description;
 $description = '';
 get_header();
 
-// グローバルネットワーク投稿一覧を取得
-$globalnetwork_posts = get_posts( array(
+// ページネーション用に現在のページ番号を取得
+$paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+
+// グローバルネットワーク投稿一覧を取得（WP_Queryでページネーション対応）
+$globalnetwork_query = new WP_Query( array(
     'post_type'      => 'globalnetwork',
-    'posts_per_page' => -1,
+    'posts_per_page' => 12,
+    'paged'          => $paged,
     'orderby'        => 'date',
     'order'          => 'DESC',
 ) );
@@ -59,15 +63,15 @@ $globalnetwork_posts = get_posts( array(
                         <div class="archive">
 
                             <ul class="archive__list">
-                            <?php if ( $globalnetwork_posts ) : ?>
-                                <?php foreach ( $globalnetwork_posts as $gn_post ) : ?>
+                            <?php if ( $globalnetwork_query->have_posts() ) : ?>
+                                <?php while ( $globalnetwork_query->have_posts() ) : $globalnetwork_query->the_post(); ?>
                                 <li class="archive__item">
-                                    <a href="<?php echo esc_url( get_permalink( $gn_post->ID ) ); ?>" class="archive__link">
+                                    <a href="<?php the_permalink(); ?>" class="archive__link">
                                     <div class="archive__image-wrapper">
-                                        <?php if ( has_post_thumbnail( $gn_post->ID ) ) : ?>
+                                        <?php if ( has_post_thumbnail() ) : ?>
                                         <img
-                                            src="<?php echo esc_url( get_the_post_thumbnail_url( $gn_post->ID, 'medium_large' ) ); ?>"
-                                            alt="<?php echo esc_attr( get_the_title( $gn_post->ID ) ); ?>"
+                                            src="<?php echo esc_url( get_the_post_thumbnail_url( null, 'medium_large' ) ); ?>"
+                                            alt="<?php echo esc_attr( get_the_title() ); ?>"
                                             class="archive__image">
                                         <?php else : ?>
                                         <img
@@ -78,15 +82,16 @@ $globalnetwork_posts = get_posts( array(
                                     </div>
                                     <div class="archive__text-wrapper">
                                         <p class="archive__title">
-                                        <?php echo esc_html( get_the_title( $gn_post->ID ) ); ?>
+                                        <?php the_title(); ?>
                                         </p>
                                         <p class="archive__text">
-                                        <?php echo esc_html( get_the_date( 'Y.m.d', $gn_post->ID ) ); ?>
+                                        <?php echo esc_html( get_the_date( 'Y.m.d' ) ); ?>
                                         </p>
                                     </div>
                                     </a>
                                 </li>
-                                <?php endforeach; ?>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
                             <?php else : ?>
                                 <li class="archive__item">
                                 <div class="archive__text-wrapper">
@@ -95,6 +100,8 @@ $globalnetwork_posts = get_posts( array(
                                 </li>
                             <?php endif; ?>
                             </ul>
+
+                            <?php ts_render_pagination( $globalnetwork_query ); ?>
 
                         </div>
                     </div>
