79bf23cda724530d90c93941737d7f2d01dbf45e
KosukeIsogai <kouiso@ritmo.co.jp>
2026-02-02 19:22:45 +0900

    Remove debug console log from pageTop.js

--- wp-content/themes/muashi/src/js/modules/pageTop.js
+++ wp-content/themes/muashi/src/js/modules/pageTop.js
@@ -7,8 +7,6 @@
  * - smoothScroll.jsのページトップスクロール機能を活用
  */
 
-console.log('DEBUG: pageTop.js module loaded');
-
 document.addEventListener('DOMContentLoaded', () => {
   const pageTopButton = document.querySelector('.js-page-top');
 
