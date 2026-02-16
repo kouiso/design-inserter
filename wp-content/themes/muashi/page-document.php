5827fb61f3f168e2ceb24f34d18c38b6c33ba4f1
KosukeIsogai <kouiso@ritmo.co.jp>
2026-02-16 22:01:37 +0900

    fix: 検索ボックスのplaceholderを「製品詳細で検索」に変更

--- wp-content/themes/muashi/page-document.php
+++ wp-content/themes/muashi/page-document.php
@@ -74,7 +74,7 @@ if ( have_posts() ) {
                         <div class="download__controls">
                           <label class="download__search">
                             <span class="download__search-label">製品タイトルで検索</span>
-                            <input type="search" class="download__search-input" data-download-search placeholder="製品名やキーワードで検索">
+                            <input type="search" class="download__search-input" data-download-search placeholder="製品詳細で検索">
                           </label>
                         </div>
 
