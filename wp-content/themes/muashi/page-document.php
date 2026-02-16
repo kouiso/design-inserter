69744b9d5eee6606f8d21fd8c5efc5a63d06dd3c
KosukeIsogai <kouiso@ritmo.co.jp>
2026-02-16 17:05:27 +0900

    fix: カタログダウンロード検索ページのラベルを「製品タイトルで検索」に変更

--- wp-content/themes/muashi/page-document.php
+++ wp-content/themes/muashi/page-document.php
@@ -73,7 +73,7 @@ if ( have_posts() ) {
                       <div class="download__main download__main--full">
                         <div class="download__controls">
                           <label class="download__search">
-                            <span class="download__search-label">キーワード</span>
+                            <span class="download__search-label">製品タイトルで検索</span>
                             <input type="search" class="download__search-input" data-download-search placeholder="製品名やキーワードで検索">
                           </label>
                         </div>
