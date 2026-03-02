1d6bfcefb30f2c5891fb55470b574c502f0373f7
KosukeIsogai <kouiso@ritmo.co.jp>
2026-03-02 10:45:54 +0900

    feat: reCAPTCHAの設定に関するコメントを追加し、不要なスクリプトの除外処理を削除 feat: 製品名検索ラベルの説明を改善

--- wp-content/themes/muashi/page-document-featured.php
+++ wp-content/themes/muashi/page-document-featured.php
@@ -71,7 +71,7 @@ if ( have_posts() ) {
                       <div class="download__main download__main--full">
                         <div class="download__controls">
                           <label class="download__search">
-                            <span class="download__search-label">製品名&#40;カタカナ&#41;</span>
+                            <span class="download__search-label">製品名 （カタカナ） で目的の製品を絞り込めます。</span>
                             <input type="search" class="download__search-input" data-download-search placeholder="製品名・キーワードで検索">
                           </label>
                         </div>
