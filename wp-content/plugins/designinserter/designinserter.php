103f5866f05b907e729e3feae95cef4e5cfb5890
KosukeIsogai <kouiso@ritmo.co.jp>
2026-05-04 01:22:10 -0700

    feat: prepare design inserter v0.2.0

--- wp-content/plugins/designinserter/designinserter.php
+++ wp-content/plugins/designinserter/designinserter.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Design Inserter
 Description: pote-chil.com/css-stock/ja の CSS パーツを WordPress 投稿に挿入するプラグイン
-Version: 0.1.0
+Version: 0.2.0
 Author: ritmo-inc
 */
 
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DESIGNINSERTER_VERSION', '0.1.0' );
+define( 'DESIGNINSERTER_VERSION', '0.2.0' );
 define( 'DESIGNINSERTER_PLUGIN_FILE', __FILE__ );
 define( 'DESIGNINSERTER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DESIGNINSERTER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
--- wp-content/plugins/designinserter/includes/render.php
+++ wp-content/plugins/designinserter/includes/render.php
@@ -6,6 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function designinserter_render_part( $part_id ) {
 	static $rendered_styles = array();
+	static $rendered_instances = 0;
 
 	$part = designinserter_get_part( $part_id );
 	if ( ! $part ) {
@@ -17,6 +18,9 @@ function designinserter_render_part( $part_id ) {
 	$html   = isset( $part['html'] ) ? $part['html'] : '';
 	$css    = isset( $part['css'] ) ? $part['css'] : '';
 	$source = isset( $part['sourceUrl'] ) ? esc_url( $part['sourceUrl'] ) : esc_url( DESIGNINSERTER_SOURCE_URL );
+	$rendered_instances++;
+	$scope = sprintf( 'di-%s-%d', $id, $rendered_instances );
+	$html  = designinserter_scope_interactive_html( $html, $scope );
 
 	$style = '';
 	if ( '' !== trim( $css ) && ! isset( $rendered_styles[ $id ] ) ) {
@@ -31,6 +35,41 @@ function designinserter_render_part( $part_id ) {
 		$style,
 		$id,
 		$title,
+		$html
+	);
+}
+
+function designinserter_scope_interactive_html( $html, $scope ) {
+	if ( '' === $html || false === strpos( $html, '=' ) ) {
+		return $html;
+	}
+
+	$scope = sanitize_html_class( $scope );
+	$id_map = array();
+
+	return preg_replace_callback(
+		'/\b(id|for|name)=(["\'])([^"\']+)\2/',
+		function ( $matches ) use ( $scope, &$id_map ) {
+			$attribute = $matches[1];
+			$quote     = $matches[2];
+			$value     = $matches[3];
+
+			if ( '' === $value ) {
+				return $matches[0];
+			}
+
+			if ( 'name' === $attribute ) {
+				return sprintf( '%s=%s%s__%s%s', $attribute, $quote, $value, $scope, $quote );
+			}
+
+			if ( 'id' === $attribute ) {
+				$id_map[ $value ] = $value . '__' . $scope;
+				return sprintf( '%s=%s%s%s', $attribute, $quote, $id_map[ $value ], $quote );
+			}
+
+			$scoped_value = isset( $id_map[ $value ] ) ? $id_map[ $value ] : $value . '__' . $scope;
+			return sprintf( '%s=%s%s%s', $attribute, $quote, $scoped_value, $quote );
+		},
 		$html
 	);
 }
